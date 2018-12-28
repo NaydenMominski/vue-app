9f992b1fa90be8f662f9414fdb1d4cb7f43a6443
NaydenMominski <mominski@abv.bg>
2018-12-28 22:38:58 +0200

    create master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>My Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
+    <style>body { padding-top: 40px; }</style>
+</head>
+
+<body>
+    <div id="app" class="container">
+        @include ('projects.list')
+
+        @yield('content')
+    </div>
+
+    <script src="/js/app.js"></script>
+</body>
+</html>
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
-    <style>body { padding-top: 40px; }</style>
-</head>
+@extends('layouts.master')
 
-<body>
-    <div id="app" class="container">
-        @include ('projects.list')
-
+@section('content')
         <form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
             <div class="control">
                 <label for="name" class="label">Project Name:</label>
@@ -33,10 +22,4 @@
                 <button class="button is-primary" :disabled="form.errors.any()">Create</button>
             </div>
         </form>
-    </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.3/axios.js"></script>
-    <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-    <script src="/js/app.js"></script>
-</body>
-</html>
+@endsection
